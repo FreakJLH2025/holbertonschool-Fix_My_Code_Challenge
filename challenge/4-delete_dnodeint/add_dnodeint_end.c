c486555ae67e6d9d9a1b79dd50120cd6c2080e76
FreakJLH2025 <joshua.luja@gmail.com>
2026-06-02 20:55:12 +0000

    add_dnodeint_end.c

---
+++ challenge/4-delete_dnodeint/add_dnodeint_end.c
@@ -0,0 +1,30 @@
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+    dlistint_t *new, *l;
+
+    if (head == NULL)
+        return (NULL);
+
+    new = malloc(sizeof(dlistint_t));
+    if (new == NULL)
+        return (NULL);
+
+    new->n = n;
+    new->next = NULL;
+
+    if (*head == NULL)
+    {
+        new->prev = NULL;
+        *head = new;
+        return (new);
+    }
+
+    l = *head;
+    while (l->next != NULL)
+        l = l->next;
+
+    l->next = new;
+    new->prev = l;
+
+    return (new);
+}
